21e9e38b11dc0501a703e78e503ca5383f5e2edd
antoforestello <anto.forestello@gmail.com>
2022-12-22 12:18:04 -0300

    Crear cuenta e iniciar sesión con email y contraseña usando Firebase

--- app/src/main/java/com/example/contratame/Actividades/CrearCuenta.java
+++ app/src/main/java/com/example/contratame/Actividades/CrearCuenta.java
@@ -1,21 +1,35 @@
 package com.example.contratame.Actividades;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.contratame.databinding.ActivityCrearCuentaBinding;
 import com.example.contratame.databinding.ActivityResutladosServiciosBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CrearCuenta extends AppCompatActivity {
 
     private ActivityCrearCuentaBinding binding;
     private TextView iniciarSesion;
     private Context ctx;
+    private String email="";
+    private String password="";
+    private FirebaseAuth firebaseAuth;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -25,6 +39,8 @@ public class CrearCuenta extends AppCompatActivity {
         setContentView(view);
         ctx=this;
 
+        firebaseAuth = FirebaseAuth.getInstance();
+
         iniciarSesion = binding.tvIniciaSesion;
         iniciarSesion.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,5 +49,47 @@ public class CrearCuenta extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+
+        binding.btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validarCampos();
+            }
+        });
+    }
+
+    private void validarCampos() {
+        email= binding.txtEmaillCrear.getText().toString().trim();
+        password = binding.txtContrasenaCrear.getText().toString().trim();
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.txtEmaillCrear.setError("Formato de email inválido");
+        }else if(TextUtils.isEmpty(password)){
+            binding.txtContrasenaCrear.setError("Ingrese una contraseña");
+        }else if(password.length()<6){
+            binding.txtContrasenaCrear.setError("La contraseña debe tener al menos 6 caracteres");
+        }else{
+            firebaseCrearCuenta();
+        }
+
+    }
+
+    private void firebaseCrearCuenta() {
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(CrearCuenta.this, "Su cuenta fue creada con éxito", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent( ctx,MainActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(CrearCuenta.this, "Se ha producido un error, vuelva a intentarlo más tarde", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
--- app/src/main/java/com/example/contratame/Actividades/IniciarSesion.java
+++ app/src/main/java/com/example/contratame/Actividades/IniciarSesion.java
@@ -1,15 +1,23 @@
 package com.example.contratame.Actividades;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.contratame.databinding.ActivityIniciarSesionBinding;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class IniciarSesion extends AppCompatActivity {
 
@@ -17,6 +25,9 @@ public class IniciarSesion extends AppCompatActivity {
     private Button btnLogin;
     private Context ctx;
     private TextView registrate;
+    private String email="";
+    private String password="";
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,12 +37,13 @@ public class IniciarSesion extends AppCompatActivity {
         setContentView(view);
         ctx=this;
 
+        firebaseAuth= FirebaseAuth.getInstance();
+
         btnLogin = binding.btnIniciarSesion;
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent( ctx,MainActivity.class);
-                startActivity(intent);
+                validarCampos();
             }
         });
 
@@ -44,6 +56,50 @@ public class IniciarSesion extends AppCompatActivity {
             }
         });
 
+        binding.btnGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
+
+
+
+    }
+
+    private void validarCampos() {
+        email= binding.txtEmail.getText().toString().trim();
+        password = binding.txtContrasena.getText().toString().trim();
+
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            binding.txtEmail.setError("Formato de email inválido");
+        }else if(TextUtils.isEmpty(password)){
+            binding.txtContrasena.setError("Ingrese una contraseña");
+        }else if(password.length()<6){
+            binding.txtContrasena.setError("La contraseña debe tener al menos 6 caracteres");
+        }else{
+            firebaseIniciarSesion();
+        }
+
+    }
+
+
+    private void firebaseIniciarSesion() {
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        Toast.makeText(IniciarSesion.this, "Se ha iniciado sesión con éxito", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent( ctx,MainActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(IniciarSesion.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
     }
 }
