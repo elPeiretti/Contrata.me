7def409656f1db90e706c99b64a891866716455e
elPeiretti <tomaspeiretti@gmail.com>
2023-01-30 10:41:55 -0300

    creacion PrestadorRF

--- app/src/main/java/com/efp/contratame/ar/modelo/Prestador.java
+++ app/src/main/java/com/efp/contratame/ar/modelo/Prestador.java
@@ -1,23 +1,33 @@
 package com.efp.contratame.ar.modelo;
 
 import java.util.List;
+import java.util.UUID;
 
 public class Prestador {
-    private String Nombre;
+
+    private UUID idPrestador;
+    private String nombre;
     private List<Servicio> serviciosBrindados;
     private String imagenPerfil;
 
     public Prestador(String nombre, String imagenPerfil) {
-        Nombre = nombre;
+        this.nombre = nombre;
+        this.imagenPerfil = imagenPerfil;
+    }
+
+    public Prestador(UUID idPrestador, String nombre, List<Servicio> serviciosBrindados, String imagenPerfil) {
+        this.idPrestador = idPrestador;
+        this.nombre = nombre;
+        this.serviciosBrindados = serviciosBrindados;
         this.imagenPerfil = imagenPerfil;
     }
 
     public String getNombre() {
-        return Nombre;
+        return nombre;
     }
 
     public void setNombre(String nombre) {
-        Nombre = nombre;
+        this.nombre = nombre;
     }
 
     public List<Servicio> getServiciosBrindados() {
