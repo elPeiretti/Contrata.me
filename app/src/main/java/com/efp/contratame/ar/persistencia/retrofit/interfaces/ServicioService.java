da1ea07fd884441e2568fc7478be6e7a48c56e61
elPeiretti <tomaspeiretti@gmail.com>
2023-01-30 19:27:39 -0300

    Creacion ServicioService

---
+++ app/src/main/java/com/efp/contratame/ar/persistencia/retrofit/interfaces/ServicioService.java
@@ -0,0 +1,16 @@
+package com.efp.contratame.ar.persistencia.retrofit.interfaces;
+
+import com.efp.contratame.ar.persistencia.retrofit.entity.ServicioRF;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ServicioService {
+
+    @GET("servicio.json")
+    Call<List<ServicioRF>> getAllServiciosDelTipo(@Query("idTipoServicio") String keyTipoServicio);
+
+}
