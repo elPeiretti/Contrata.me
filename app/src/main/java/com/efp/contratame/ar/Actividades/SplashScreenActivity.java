f7c88161b47340046b568ba243a11954687c3759
clari62 <clariespertino62@gmail.com>
2023-02-03 15:26:33 -0300

    mayor duración a splash screen y cambio de transiciones

--- app/src/main/java/com/efp/contratame/ar/Actividades/SplashScreenActivity.java
+++ app/src/main/java/com/efp/contratame/ar/Actividades/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package com.efp.contratame.ar.Actividades;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -12,6 +13,7 @@ public class SplashScreenActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SystemClock.sleep(2000);
         startActivity(new Intent(SplashScreenActivity.this, IniciarSesion.class));
         finish();
     }
