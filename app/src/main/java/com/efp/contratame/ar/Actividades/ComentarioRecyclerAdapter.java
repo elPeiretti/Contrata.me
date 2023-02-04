44070a30502e97f7bd4030fbdc98adb704b5a104
elPeiretti <tomaspeiretti@gmail.com>
2023-02-04 18:10:09 -0300

    creacion del adapter para el recycler

---
+++ app/src/main/java/com/efp/contratame/ar/Actividades/ComentarioRecyclerAdapter.java
@@ -0,0 +1,62 @@
+package com.efp.contratame.ar.Actividades;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import com.efp.contratame.ar.R;
+import com.efp.contratame.ar.modelo.Comentario;
+import java.util.List;
+
+public class ComentarioRecyclerAdapter extends RecyclerView.Adapter<ComentarioRecyclerAdapter.ComentarioViewHolder>{
+
+    private List<Comentario> comentarioList;
+    private final LayoutInflater mInflater;
+
+    ComentarioRecyclerAdapter(Context ctx, List<Comentario> dataSet){
+        this.mInflater = LayoutInflater.from(ctx);
+        this.comentarioList = dataSet;
+    }
+
+    @NonNull
+    @Override
+    public ComentarioViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return new ComentarioViewHolder(mInflater.inflate(R.layout.fila_comentario, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ComentarioViewHolder holder, int position) {
+        holder.usuario.setText(comentarioList.get(position).getUsuario());
+        holder.contenido.setText(comentarioList.get(position).getContenido());
+        holder.fecha.setText(comentarioList.get(position).getFecha().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return comentarioList.size();
+    }
+
+    public void updateData(List<Comentario> comentarios) {
+        comentarioList.clear();
+        comentarioList.addAll(comentarios);
+        notifyDataSetChanged();
+    }
+
+    public static class ComentarioViewHolder extends RecyclerView.ViewHolder {
+
+        TextView fecha;
+        TextView usuario;
+        TextView contenido;
+
+        public ComentarioViewHolder(@NonNull View itemView) {
+            super(itemView);
+            usuario = itemView.findViewById(R.id.tv_usuario);
+            contenido = itemView.findViewById(R.id.tv_desc_comentario);
+            fecha = itemView.findViewById(R.id.tv_fecha_comm);
+        }
+    }
+
+}
