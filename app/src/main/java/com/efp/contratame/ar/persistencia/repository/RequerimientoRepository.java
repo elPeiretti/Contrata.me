f9646c3b034aacc6f0746bc2f85c719ae5d4089d
elPeiretti <tomaspeiretti@gmail.com>
2023-02-10 18:10:17 -0300

    creacion RequerimientoRepository

---
+++ app/src/main/java/com/efp/contratame/ar/persistencia/repository/RequerimientoRepository.java
@@ -0,0 +1,32 @@
+package com.efp.contratame.ar.persistencia.repository;
+
+import com.efp.contratame.ar.modelo.Requerimiento;
+import com.efp.contratame.ar.persistencia.datasource.RequerimientoDataSource;
+import com.efp.contratame.ar.persistencia.datasource.TipoServicioDataSource;
+import com.efp.contratame.ar.persistencia.retrofit.RequerimientoRetrofitDataSource;
+import com.efp.contratame.ar.persistencia.retrofit.TipoServicioRetrofitDataSource;
+
+import java.util.UUID;
+
+public class RequerimientoRepository implements RequerimientoDataSource {
+
+    private final RequerimientoDataSource reqDataSource;
+
+    private RequerimientoRepository(RequerimientoDataSource reqDataSource){
+        this.reqDataSource = reqDataSource;
+    }
+
+    public static RequerimientoDataSource createInstance(){
+        return new RequerimientoRepository(new RequerimientoRetrofitDataSource());
+    }
+
+    @Override
+    public void saveRequerimiento(Requerimiento req) {
+        reqDataSource.saveRequerimiento(req);
+    }
+
+    @Override
+    public void getAllRequerimientosFrom(UUID idUsuario) {
+        reqDataSource.getAllRequerimientosFrom(idUsuario);
+    }
+}
