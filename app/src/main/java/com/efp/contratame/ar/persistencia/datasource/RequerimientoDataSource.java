950a24e28de37e72f84fd33eeed4481167aa538d
elPeiretti <tomaspeiretti@gmail.com>
2023-02-10 18:05:39 -0300

    otro metodo para RequerimientoDataSource

--- app/src/main/java/com/efp/contratame/ar/persistencia/datasource/RequerimientoDataSource.java
+++ app/src/main/java/com/efp/contratame/ar/persistencia/datasource/RequerimientoDataSource.java
@@ -2,12 +2,20 @@ package com.efp.contratame.ar.persistencia.datasource;
 
 import com.efp.contratame.ar.modelo.Requerimiento;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface RequerimientoDataSource {
 
-    interface SaveRequerimientoCallback{
+    interface SaveRequerimientoCallback {
         void onError();
         void onResult();
     }
+    interface GetAllRequerimientosByFromCallback {
+        void onError();
+        void onResult(List<Requerimiento> requerimientoList);
+    }
 
     void saveRequerimiento(Requerimiento req);
+    void getAllRequerimientosFrom(UUID idUsuario);
 }
