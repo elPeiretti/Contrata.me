4f1969193b797d38e1dc5f7fa27d26500853b76b
elPeiretti <tomaspeiretti@gmail.com>
2023-02-11 12:06:52 -0300

    implementacion de los metodos de RequerimientoDataSource

--- app/src/main/java/com/efp/contratame/ar/persistencia/retrofit/RequerimientoRetrofitDataSource.java
+++ app/src/main/java/com/efp/contratame/ar/persistencia/retrofit/RequerimientoRetrofitDataSource.java
@@ -1,15 +1,24 @@
 package com.efp.contratame.ar.persistencia.retrofit;
 
+import android.util.Log;
+
 import androidx.annotation.NonNull;
 
 import com.efp.contratame.ar.modelo.Requerimiento;
+import com.efp.contratame.ar.modelo.TipoServicio;
 import com.efp.contratame.ar.persistencia.datasource.RequerimientoDataSource;
+import com.efp.contratame.ar.persistencia.datasource.TipoServicioDataSource;
+import com.efp.contratame.ar.persistencia.repository.TipoServicioRepository;
 import com.efp.contratame.ar.persistencia.retrofit.entity.RequerimientoRF;
+import com.efp.contratame.ar.persistencia.retrofit.entity.ServicioRF;
 import com.efp.contratame.ar.persistencia.retrofit.interfaces.RequerimientoService;
 import com.efp.contratame.ar.persistencia.retrofit.mapper.RequerimientoMapper;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import okhttp3.OkHttpClient;
@@ -43,19 +52,54 @@ public class RequerimientoRetrofitDataSource implements RequerimientoDataSource 
                 .saveRequerimiento(idUsuario.toString(), RequerimientoMapper.toEntity(req));
         reqAsyn.enqueue(new Callback<RequerimientoRF>() {
             @Override
-            public void onResponse(Call<RequerimientoRF> call, Response<RequerimientoRF> response) {
-
+            public void onResponse(@NonNull Call<RequerimientoRF> call, @NonNull Response<RequerimientoRF> response) {
+                if (response.code() == 200){
+                    callback.onResult();
+                }
+                else{
+                    callback.onError();
+                }
             }
 
             @Override
-            public void onFailure(Call<RequerimientoRF> call, Throwable t) {
-
+            public void onFailure(@NonNull Call<RequerimientoRF> call, @NonNull Throwable t) {
+                callback.onError();
             }
         });
     }
 
     @Override
     public void getAllRequerimientosFrom(UUID idUsuario, GetAllRequerimientosFromCallback callback) {
-        //TODO
+        TipoServicioRepository.createInstance().getAllTipoServicios(new TipoServicioDataSource.GetAllTipoServiciosCallback() {
+            @Override
+            public void onError() {
+                callback.onError();
+            }
+
+            @Override
+            public void onResult(List<TipoServicio> tipos) {
+                Call<Map<String,RequerimientoRF>> reqAsyn = requerimientoService.getAllRequerimientosFrom(idUsuario.toString());
+
+                Log.i("RETRO",reqAsyn.request().url().toString());
+                reqAsyn.enqueue(new Callback<Map<String, RequerimientoRF>>() {
+                    @Override
+                    public void onResponse(@NonNull Call<Map<String, RequerimientoRF>> call, @NonNull Response<Map<String, RequerimientoRF>> response) {
+
+                        if (response.code() == 200 && response.body() != null) {
+                            Map<String, RequerimientoRF> data = response.body();
+                            callback.onResult(RequerimientoMapper.fromEntities(new ArrayList<>(data.values()), tipos));
+                        }
+                        else{
+                            callback.onError();
+                        }
+                    }
+
+                    @Override
+                    public void onFailure(@NonNull Call<Map<String, RequerimientoRF>> call, @NonNull Throwable t) {
+
+                    }
+                });
+            }
+        });
     }
 }
